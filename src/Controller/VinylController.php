af4fd859c6f12bab1c810a9de325a418ff821121
saman65 <samansalimi65@gmail.com>
2023-09-05 20:27:21 +0200

    adding node libraries

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,13 +1,15 @@
 <?php
 namespace App\Controller;
+use Twig\Environment;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Loader\Configurator\twig;
 
 class VinylController extends AbstractController
 {
     #[Route('/', name: 'app_homepage', priority: 2)]
-    public function homepage(): Response
+    public function homepage(Environment $twig): Response
     {
         $tracks = [
             ['song' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
@@ -18,17 +20,19 @@ class VinylController extends AbstractController
             ['song' => 'Fantasy', 'artist' => 'Mariah Carey'],
         ];
         //dd($tracks); {This is a Debug function similar to dump(). Both are useful to test. Dump() performs hiddenly but can be revealed}
-        return $this->render('vinyl/homepage.html.twig', [
+        $html = $twig->render('vinyl/homepage.html.twig', [
             'title' => 'PB & Jams',
             'tracks' => $tracks,
         ]);
+        return new Response($html);
+        #$this->render() is working inside the controllers. But $twig->render is working also outside the controllers#
     }
 
     #[Route('/browse/{slug}', name: 'app_browse')]
     public function browse(string $slug = null): Response
     {
         if ($slug) {
-            $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
+            $title = 'Genre: '.str_replace('-', ' ', $slug);
         } else {
             $title = 'All Genres';
         }
