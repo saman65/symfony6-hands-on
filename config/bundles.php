2ca8e93c291bd1ba6dced7737d23560e77b6b622
saman65 <samansalimi65@gmail.com>
2023-10-05 01:18:17 +0200

    sensio/framework-extra-bundle was removed as recommended by symfony and the other components were updated
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   composer.json
    	modified:   composer.lock
    	modified:   config/bundles.php
    	deleted:    config/packages/sensio_framework_extra.yaml
    	modified:   symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -9,7 +9,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
